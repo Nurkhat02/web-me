999241f0e55e228d148a0d5eac32439f324c950f
Nurkhat02 <78536893+Nurkhat02@users.noreply.github.com>
2022-05-30 21:59:31 +0600

    Update ex1.js

--- js/ex1.js
+++ js/ex1.js
@@ -3,5 +3,5 @@ let inputElement_name = document.querySelector(".input_Name");
 let inputElement_Surename = document.querySelector(".input_Surename");
 let resultElement = document.querySelector(".result")
 btnElement.addEventListener("click", function(){
-    resultElement.textContent = "Здравствуйте, " +  inputElement_name.value + " " + inputElement_Surename.value; 
+    resultElement.textContent =  (`Здравствуйте,  ${inputElement_name.value} ${inputElement_Surename.value}!`); 
 })
